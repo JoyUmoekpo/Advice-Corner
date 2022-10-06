5213503cbf1e3470fdd29f159e9582125f33b426
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-10-06 08:04:18 -0500

    Added query selector for greeting div

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,13 @@
 fetch('https://api.adviceslip.com/advice')
     .then(response => response.json())
     .then(data => {
+
+        document.querySelector('#greeting')
+        .innerHTML =`Hello! Here's your advice:`;
+
         document.querySelector('#advice')
-        .innerHTML =`<h6>Here's your advice: <br> <br> ${data.slip.advice}</h6>`
+        .innerHTML =`${data.slip.advice}`;
+
     })
     .catch(() => console.log('An error occurred. Please fix your code.'));
 
