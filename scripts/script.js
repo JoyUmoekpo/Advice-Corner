d79e76dc038cc5948c68f51419d045bb32d7bf82
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-10-05 21:55:06 -0500

    Fixed async / await fetch

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,12 @@
 async function getAdvice(){
-    let url = 'https://api.adviceslip.com/advice';
+    let url = "https://api.adviceslip.com/advice";
     try {
         let response = await fetch(url);
-        return await response.json();
+        return response.json();
     } catch(error) {
         console.log(error);
     }
 }
 
-async function readAdvice(){
-    let advice = await getAdvice();
-    let html = '';
-    advice.forEach(adv => {
-        let htmlSegment = `<div class="advice">
-                            <h1 class="advice-id">${adv.slip_id}</h1>
-                            <h2 class="advice-message">${adv.advice}</h2>
-                        </div>`;
-        html += htmlSegment;
-    });
-
-    let container = document.querySelector('.container');
-    container.innerHTML = html;
-}
-
-readAdvice();
+getAdvice()
+.then(json => console.log(json));
