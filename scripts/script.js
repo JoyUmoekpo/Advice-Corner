fbfab1e1632d334c5ffe7b814baa24cefbcf1ab4
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-10-05 20:25:22 -0500

    Added different fetches

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,26 @@
-fetch('https://api.adviceslip.com/advice')
-.then((response) => response.json())
-.then((data) => {
-    console.log(data)
-})
-.catch((error) => {
-    console.log('An error occurred: ', error)
-});
+// fetch('https://api.adviceslip.com/advice')
+// .then((response) => response.json())
+// .then((data) => {
+//     console.log(data)
+// })
+// .catch((error) => {
+//     console.log('An error occurred: ', error)
+// });
+
+// fetch('https://api.adviceslip.com/advice/36')
+// .then((response) => response.json())
+// .then((data) => {
+//     console.log(data)
+// })
+// .catch((error) => {
+//     console.log('An error occurred: ', error)
+// });
+
+// fetch('https://api.adviceslip.com/advice/search/high')
+// .then((response) => response.json())
+// .then((data) => {
+//     console.log(data)
+// })
+// .catch((error) => {
+//     console.log('An error occurred: ', error)
+// });
