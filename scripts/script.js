0807c3cef1a45bb22815826e434e8c5fa9938b4f
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-10-06 01:58:22 -0500

    Added break in innerHTML

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ fetch('https://api.adviceslip.com/advice')
     .then(response => response.json())
     .then(data => {
         document.querySelector('#advice')
-        .innerHTML =`<h6>Here's your advice: \n ${data.slip.advice}</h6>`
+        .innerHTML =`<h6>Here's your advice: <br> ${data.slip.advice}</h6>`
     })
     .catch(() => console.log('An error occurred. Please fix your code.'));
 
