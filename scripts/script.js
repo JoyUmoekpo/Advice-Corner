b3203ba905dc2727d5398c80af493844a367cabb
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-10-06 00:15:27 -0500

    div change

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,33 @@
-async function getAdvice(){
-    let url = "https://api.adviceslip.com/advice";
-    try {
-        let response = await fetch(url);
+request = (user_input) => {
+    let url = 'https://api.adviceslip.com/advice';
+    fetch(url)
+    .then((response) => {
+        if (!response.ok) throw Error(response.statusText);
         return response.json();
-    } catch(error) {
-        console.log(error);
-    }
+    })
+    .then(data => {
+        document.querySelector("#advice").textContent = data.json();
+    })
+    .catch((error) => document.querySelector("#advice").textContent = error);
 }
 
-getAdvice()
-.then(data => console.log(data));
+
+
+
+
+// async function getAdvice() {
+//     let url = "https://api.adviceslip.com/advice";
+//     try {
+//         let response = await fetch(url);
+//         return response.json();
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// getAdvice()
+//     .then(data => console.log(data));
+
+// fetch('https://api.adviceslip.com/advice')
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
