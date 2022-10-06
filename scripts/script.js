df43ec3655f4b1088b4118ecfc9c50dc98a8b9ee
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-10-06 00:38:26 -0500

    FIGURED OUT CORRECT API SYNTAX

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 fetch('https://api.adviceslip.com/advice')
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    // .then((data) => console.log(data))
+    .then((data) => console.log(data.slip.id, data.slip.advice))
     .catch(() => console.log('An error occurred. Please fix your code.'));
 
