d5b2333739310060718035d41f12bb9214a9d353
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-10-05 20:29:57 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
-// fetch('https://api.adviceslip.com/advice')
-// .then((response) => response.json())
-// .then((data) => {
-//     console.log(data)
-// })
-// .catch((error) => {
-//     console.log('An error occurred: ', error)
-// });
+fetch('https://api.adviceslip.com/advice')
+.then((response) => response.json())
+.then((data) => {
+    console.log(data)
+})
+.catch((error) => {
+    console.log('An error occurred: ', error)
+});
 
 // fetch('https://api.adviceslip.com/advice/36')
 // .then((response) => response.json())
