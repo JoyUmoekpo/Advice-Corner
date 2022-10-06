933093c6bf5b609afbf854cc2ed91701423ab697
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-10-06 00:27:52 -0500

    Added error message

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
 fetch('https://api.adviceslip.com/advice')
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => console.log(data))
+    .catch(() => console.log('An error occurred. Please fix your code.'));
+
